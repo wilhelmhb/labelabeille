e41f0a28dee70be846505a793cf0313673e2bfa1
wilhelmbh <wilhelmhb@live.fr>
2016-02-18 15:21:32 +0100

    fonctions pour la page de chargement

---
+++ LabelAbeille/platforms/android/assets/www/js/chargement.js
@@ -0,0 +1,50 @@
+if(window.addEventListener){
+    window.addEventListener('load', debutChargement, false);
+}else{
+    window.attachEvent('onload', debutChargement);
+}
+
+function debutChargement(){
+	dessinCercleFLAT(_("canvasProgressSimple"),87);
+}
+
+function dessinCercleFLAT(cible,pourcentage){
+ var dateActu = new Date();
+ timeActu= dateActu.getTime();
+ 
+ var bg = cible;
+ var ctx = bg.getContext('2d');
+ // netoyage du canvas existant
+ var cote = _("alveoles_chargement").width/3;
+ _("canvasProgressSimple").width=_("canvasProgressSimple").height=cote+2;
+ _("canvasProgressSimple").style.top='calc( 50% - '+(cote/2)+'px + '+ (_("entete1").offsetHeight/2 + 2)+'px)';
+ _("canvasProgressSimple").style.left='calc(50% - '+cote/2+'px)';
+ ctx.clearRect ( 0 , 0 , cote , cote );
+ // definition d'un cercle complet
+ var circ = Math.PI * 2;
+ var quart = Math.PI / 2;
+ 
+ // creation d'un cercle de progression
+ var ctx = ctx = bg.getContext('2d');
+ ctx.strokeStyle = '#e1ffbc';
+ ctx.lineWidth = cote/7;
+ ctx.beginPath();
+ ctx.arc(cote/2, cote/2, cote/2-ctx.lineWidth/2, 0, circ, false);
+ ctx.stroke();
+ 
+ // creation d'un cercle de progression
+ var ctx = ctx = bg.getContext('2d');
+ ctx.strokeStyle = '#99e144';
+ ctx.lineWidth = cote/7;
+ ctx.beginPath();
+ ctx.shadowOffsetY = 0;
+ ctx.arc(cote/2, cote/2, cote/2-ctx.lineWidth/2, -(quart), ((circ) * pourcentage/100) - quart, false);
+ ctx.stroke();
+ 
+ ctx.font = "bold "+(cote/8)+"pt Calibri,Geneva,Arial";
+ ctx.textAlign = 'center';
+ ctx.fillStyle = "#000000";
+ ctx.fillText(pourcentage+'%', cote/2, cote/2+cote/20);
+ ctx.shadowBlur = 0; 
+
+}
